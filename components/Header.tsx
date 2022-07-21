56d8574aa7f919a1f0752a4e6ec49d188a036234
sercan-works <sercantas1026@gmail.com>
2022-07-21 17:02:22 +0300

    s

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+
+
+function Header() {
+  return (
+    <header>
+        <div className="flex item-center space-x-2 md:space-x-10">
+            <img 
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+            />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">Tv Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My List</li>
+            </ul>
+        </div>
+
+        <div>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
