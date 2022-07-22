51cd99ce290a50806604d4b39f95efc5dc58eada
sercan-works <sercantas1026@gmail.com>
2022-07-23 01:12:13 +0300

    vfg

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Row from '../components/Row'
+import { Movie } from '../typings'
 import requests from '../utils/requests'
 
-const Home = ({netflixOriginals}) => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+  //products: Product[]
+}
+
+
+const Home = ({ netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+  //products, 
+}: Props) => {
   console.log(netflixOriginals)
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -15,15 +39,19 @@ const Home = ({netflixOriginals}) => {
       </Head>
       
        <Header/>
-      <main>
-         <Banner/>
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+         <Banner netflixOriginals={netflixOriginals}/>
         <section>
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+         
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
           {/* {Modal} */}
@@ -57,7 +85,15 @@ export const getServerSideProps = async () => {
   ])
 return{
   props:{
-
+    netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      
   }
 }
   
