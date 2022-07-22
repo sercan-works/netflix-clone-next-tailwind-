3f223ab1fb7009c1acfa3aeace6c44bdb989673a
sercan-works <sercantas1026@gmail.com>
2022-07-22 21:11:57 +0300

    s

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,31 @@
-
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 function Header() {
+  const [isScrolled,setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0){
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll",handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll',handleScroll)
+    }
+  }, [])
+  
+
+
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex item-center space-x-2 md:space-x-10">
             <img 
             src="https://rb.gy/ulxxee"
@@ -20,8 +43,16 @@ function Header() {
             </ul>
         </div>
 
-        <div>
-
+        <div className='flex items-center space-x-4 text-sm font-light'>
+             <SearchIcon className='hidden sm:inline h-6 w-6 '/>
+             <p className='hidden lg:inline'>Kids</p>
+             <BellIcon className='h-6 w-6 '/>
+             <Link href='/account'>
+                <img 
+                src="https://rb.gy/g1pwyx" 
+                alt="" 
+                className='cursor-pointer rounded'/>
+             </Link>
         </div>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
+import requests from '../utils/requests'
 
-const Home: NextPage = () => {
+const Home = ({netflixOriginals}) => {
+  console.log(netflixOriginals)
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,7 +16,7 @@ const Home: NextPage = () => {
       
        <Header/>
       <main>
-        {/* {Banner} */}
+         <Banner/>
         <section>
           {/* {Row} */}
           {/* {Row} */}
@@ -30,3 +33,32 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+return{
+  props:{
+
+  }
+}
+  
+}
